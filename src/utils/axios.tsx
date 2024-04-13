2cfdee8df6ddec8b351d099b3c0aefddb0124e29
Naveed Shaik <naveedahammed549@gmail.com>
2024-04-13 16:10:53 +0530

    refactored

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8001/api/v1/admin";
+const BASE_URL = import.meta.env.VITE_SERVER;
 
 const headers = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
+	"Content-Type": "application/json",
+	Accept: "application/json",
 };
 
 const publicAxios = axios.create({
-  baseURL: BASE_URL,
-  headers,
+	baseURL: BASE_URL,
+	headers,
 });
 
 export const privateAxios = axios.create({
-  baseURL: BASE_URL,
-  headers,
-  withCredentials: true,
+	baseURL: BASE_URL,
+	headers,
+	withCredentials: true,
 });
 
 export default publicAxios;
