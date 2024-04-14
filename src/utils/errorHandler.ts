14baa7d32c50bc0c99f0efe60d67964021abf282
Naveed Shaik <naveedahammed549@gmail.com>
2024-04-15 01:07:09 +0530

    refctored

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -4,16 +4,18 @@ import toast from "react-hot-toast";
 export const errorHandler = (err: unknown) => {
 	if (err === null) throw new Error("Unrecoverable error!! Error is null!");
 	if (axios.isAxiosError<{ message: string }>(err)) {
+		console.log(err.code);
+		console.log(err.message);
 		if (err.code === "ERR_NETWORK") {
-			console.log("connection problems..");
 			return toast.error("Network connection problem...");
-		} else if (err.code === "ERR_CANCELED") {
-			console.log("connection canceled..");
-			toast.error("connection canceled..");
-		} else if (!err?.response) {
-			toast.error("Something went wrong");
-		} else {
-			toast.error(err.response?.data.message);
+		} else if (err.code !== "ERR_CANCELED") {
+			if (err.code === "ERR_BAD_RESPONSE") {
+				return toast.error("Something went wrong");
+			} else if (!err?.response) {
+				toast.error("Something went wrong");
+			} else {
+				toast.error(err.response?.data.message);
+			}
 		}
 	}
 };
